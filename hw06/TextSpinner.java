1cf4b1b9e5ddecc9b1c74b30c793bf3d3e9232d7
aaa218 <aaa218@lehigh.edu>
2016-03-09 01:01:48 +0000

    updated TextSpinner

--- hw06/TextSpinner.java
+++ hw06/TextSpinner.java
@@ -9,7 +9,7 @@ public class TextSpinner{
     public static void main(String [] args){
         
         while (true){
-            System.out.print("/\b");
+            System.out.print("/\b");//Printing out the symbols for the spinner including the backspaces
             System.out.print("-\b");
             System.out.print("\\\b");
             System.out.print("|\b");
